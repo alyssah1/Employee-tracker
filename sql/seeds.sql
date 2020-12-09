4ea214afd01163e0ff4c23110a21647d0da2f64f
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2020-12-09 17:34:35 -0600

    added seeds sql

---
+++ sql/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES("Alyssa", "Hollowell", 3),
+("Blake", "Wesley", 5),
+("Ashley", "Smith", 7);
+
+
+INSERT INTO departments (name)
+VALUES("Sales"),
+("Engineer"),
+("Legal"),
+("Finance");
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES("Sales Lead", 80000, 6),
+("Software engineer", 100000, 8),
+("Accountant", 90000, 10),
+("Lawyer", 120000, 4);
